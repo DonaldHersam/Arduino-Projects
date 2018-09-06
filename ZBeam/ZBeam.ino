c5b98c54f5e3fd086d8c5dac2c365f5afaa3cd31
DonaldHersam <hersam.donaldhersam@gmail.com>
2018-09-06 09:26:00 -0700

    edit to pid control loop

--- ZBeam/ZBeam.ino
+++ ZBeam/ZBeam.ino
@@ -7,12 +7,9 @@
 #include <Encoder.h>
 #include <PID_v1.h>
 
-// Change these two numbers to the pins connected to your encoder.
-//   Best Performance: both pins have interrupt capability
-//   Good Performance: only the first pin has interrupt capability
-//   Low Performance:  neither pin has interrupt capability
+
 Encoder myEnc(15,16);
-//   avoid using pins with LEDs attached
+
 
 #define screwdriver_brake 7
 #define screwdriver_dir 8
@@ -23,15 +20,15 @@ Encoder myEnc(15,16);
 #define PUL_ACTUATOR_TRANSLATION 11
 #define DIR_ACTUATOR_TRANSLATION 12
 
-#define delay_time 250
 
 
 
-double oldPosition  = -999;
+
+
 
 //PID Stuff
 double Setpoint, Input, Output;
-double Kp=.01, Ki=0.001, Kd=0.001;
+double Kp=.1, Ki=0, Kd=0;
 
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
@@ -76,84 +73,79 @@ void setup() {
 void loop() {
 
 
-//Screwdriver
-//  digitalWrite(screwdriver_brake,0);  //Turn off brake to DC motor
-//  digitalWrite(screwdriver_dir,1);  //Screw bolt in
-//  delay(1000);
-//
-//  while(1){
-//    digitalWrite(screwdriver_brake,1);
-//  }
 
 
-
-
-
-
-
-  //Touch Probe Encoder
   Input = myEnc.read();
   Input *= 0.06; //Steps to u-meter conversion
   myPID.Compute();
 
 
-  outputMotorLinear = -Output/1.5; //outputMotorLinear in units of steps
+  outputMotorLinear = Output/1.5; //outputMotorLinear in units of steps
 
 
-//  Serial.print("Input: ");
-//  Serial.println(Input);
-//  Serial.print("Output: ");
-//  Serial.println(outputMotorLinear);
-//  delay(100);
+  LinearActuator(outputMotorLinear);
 
-//  if (newPosition != oldPosition) {
-//    oldPosition = newPosition;
-//    
-////    Serial.println(newPosition);
-//  }
 
 
 //static int lastTime = 0;
 //int myTime = millis();
 //if(myTime - lastTime >= 1500){
-//  Serial.println(newPosition);
+//  Serial.print("Input: ");
+//  Serial.println(Input);
+//  Serial.print("Output: ");
+//  Serial.println(outputMotorLinear);
 //  lastTime=myTime;
 //}
 
+}
 
 
 
 
-////Linear Actuator
-//  static bool dir = 1;
-//  
-//  
-//  static int counter = 0;
-// 
-//    if (counter>=12000){
-////      dir = !dir;
-//      if (dir == 0){
-//        dir=1;
-//      }
-//      else{
-//        dir=0;
-//      }
-//      counter=0;
-      digitalWrite(DIR_ACTUATOR_TRANSLATION,1);
-////      Serial.println(dir);
-//    }
-//
-// 
-//
 
-for (int i = 0; i <= outputMotorLinear; i++){
+
+
+
+
+void LinearActuator(double motorSpeed){
+  
+  
+  if (motorSpeed > 0){
+    digitalWrite(DIR_ACTUATOR_TRANSLATION,0);
+  }
+  if (motorSpeed < 0){
+    digitalWrite(DIR_ACTUATOR_TRANSLATION,1);
+  }
+
+  int delay_time = (1/abs(motorSpeed))*150; //motor speed has units of revs per micro second
+  if (delay_time <= 250){
+    delay_time = 250;
+  }
+
+static int lastTime = 0;
+int myTime = millis();
+if(myTime - lastTime >= 1500){
+  Serial.print("motorSpeed: ");
+  Serial.println(motorSpeed);
+  Serial.print("delay_time: ");
+  Serial.println(delay_time);
+  lastTime=myTime;
+}
+  
     digitalWrite(PUL_ACTUATOR_TRANSLATION,LOW);
     delayMicroseconds(delay_time);
     digitalWrite(PUL_ACTUATOR_TRANSLATION,HIGH);
     delayMicroseconds(delay_time);
-}
-//    counter += 1;
 
-  
 }
 
+
+
+
+
+
+
+void Screwdriver(){
+  digitalWrite(screwdriver_brake,0);  //Turn off brake to DC motor
+  digitalWrite(screwdriver_dir,1);  //Screw bolt in
+}
